f34f51036c83761f0283f2f1d56a8c1330c3ec97
Jeremy Johns <jez.johns@gmail.com>
2023-02-07 13:40:11 +0000

    moving new-test-page into teched201 repo

---
+++ class-01/new-test-page/myscript.js
@@ -0,0 +1,9 @@
+function letMeKnow() {
+  alert("This is an alert. So now you know");
+}
+function askMe() {
+  asked = prompt("Ask me anything. Go on. Anything");
+}
+function debugMe() {
+  console.log("No bugs so far! Plus this is what you asked: ", asked);
+}
