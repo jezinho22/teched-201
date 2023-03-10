1abe1d718b75d9473b674c61a413f02e39af8c3c
Jeremy Johns <jez.johns@gmail.com>
2023-03-10 16:44:58 +0000

    sorted logic; applied css

--- 201-class-10/Arkwrights/app.js
+++ 201-class-10/Arkwrights/app.js
@@ -4,49 +4,48 @@ const form = document.querySelector("#form");
 // initialise array to try to store input data as objects
 foodArray = [];
 
-// initialise variables outside of any functions for scope
-let cuisine = "Chinese";
-let dish = "Chow mein";
-let weight = 500;
-
 // create class to match inputs
 class WorldFood {
-	constructor(cuisine, dish, weight) {
+	constructor(cuisine, foodItem, weight) {
 		this.cuisine = cuisine;
-		this.dish = dish;
+		this.foodItem = foodItem;
 		this.weight = weight;
 	}
 }
 
-// create new instance of WorldFood class
-var newFood = new WorldFood(cuisine, dish, weight);
+// initialise variables outside of any functions for scope
+let cuisine;
+let foodItem;
+let weight = 0;
 
 // add event listener for form, using variable containing it
 form.addEventListener("submit", () => {
 	console.log("handler working");
 	// prevent page reloading
 	event.preventDefault();
+
 	// assign form inputs to variables
 	cuisine = event.target.cuisine.value;
-	dish = event.target.dish.value;
+	foodItem = event.target.foodItem.value;
 	weight = event.target.weight.value;
-	console.log(cuisine, dish, weight);
-	// // create new instance of WorldFood class
-	// var newFood = new WorldFood(cuisine, dish, weight);
+	console.log(cuisine, foodItem, weight);
+
+	// create new instance of WorldFood class
+	var newFood = new WorldFood(cuisine, foodItem, weight);
 
 	// create row, create cells and add data
 	var row = document.createElement("tr");
 	var cuisineCell = document.createElement("td");
 	cuisineCell.innerText = newFood.cuisine;
-	var dishCell = document.createElement("td");
-	dishCell.innerText = newFood.dish;
+	var foodItemCell = document.createElement("td");
+	foodItemCell.innerText = newFood.foodItem;
 	var weightCell = document.createElement("td");
-	weightCell.innerText = newFood.weight;
+	weightCell.innerText = newFood.weight + "g";
 
 	// combine cells and then append to table body
-	// var allCells = cuisineCell.outerHTML + dishCell.outerHTML + weightCell.outerHTML;
+	// var allCells = cuisineCell.outerHTML + foodItemCell.outerHTML + weightCell.outerHTML;
 	row.appendChild(cuisineCell);
-	row.appendChild(dishCell);
+	row.appendChild(foodItemCell);
 	row.appendChild(weightCell);
 	var tbody = document.querySelector("tbody");
 	tbody.appendChild(row);
