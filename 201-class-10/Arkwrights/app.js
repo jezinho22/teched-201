5460c3d686ef3df03fb354d61bdb13f58192b2fb
Jeremy Johns <jez.johns@gmail.com>
2023-03-10 13:59:46 +0000

    none

--- 201-class-10/Arkwrights/app.js
+++ 201-class-10/Arkwrights/app.js
@@ -1,46 +1,13 @@
 // assign form element to a variable
 const form = document.querySelector("#form");
 
-var testFood = ['Chinese','Chow mein','500']
-
 // initialise array to try to store input data as objects
 foodArray = [];
 
 // initialise variables outside of any functions for scope
-let cuisine;
-let dish;
-let weight = 0;
-
-// add event listener for form, using variable containing it
-form.addEventListener("submit", () => {
-	console.log("handler working");
-	// prevent page reloading
-	event.preventDefault();
-	// assign form inputs to variables
-	cuisine = event.target.cuisine.value;
-	dish = event.target.dish.value;
-	weight = event.target.weight.value;
-    // create new instance of WorldFood class
-    var newFood = new WorldFood(cuisine, dish, weight);
-
-    // create row, create cells and add data
-    var row = document.createElement('tr');
-    var cuisineCell = document.createElement('td');
-    cuisineCell.innerText = newFood.cuisine;
-    var dishCell = document.createElement('td')
-    dishCell.innerText = newFood.dish;
-    var weightCell = document.createElement('td')
-    weightCell.innerText = newFood.weight;
-    // combine cells and then append to table body
-    // var allCells = cuisineCell.outerHTML + dishCell.outerHTML + weightCell.outerHTML;
-
-    row.appendChild(cuisineCell);
-    row.appendChild(dishCell);
-    row.appendChild(weightCell);
-    var tbody = document.querySelector('tbody')
-    tbody.appendChild(row);
-}
-});
+let cuisine = "Chinese";
+let dish = "Chow mein";
+let weight = 500;
 
 // create class to match inputs
 class WorldFood {
@@ -51,5 +18,36 @@ class WorldFood {
 	}
 }
 
-function addToFrontEnd(){
+// create new instance of WorldFood class
+var newFood = new WorldFood(cuisine, dish, weight);
 
+// add event listener for form, using variable containing it
+form.addEventListener("submit", () => {
+	console.log("handler working");
+	// prevent page reloading
+	event.preventDefault();
+	// assign form inputs to variables
+	cuisine = event.target.cuisine.value;
+	dish = event.target.dish.value;
+	weight = event.target.weight.value;
+	console.log(cuisine, dish, weight);
+	// // create new instance of WorldFood class
+	// var newFood = new WorldFood(cuisine, dish, weight);
+
+	// create row, create cells and add data
+	var row = document.createElement("tr");
+	var cuisineCell = document.createElement("td");
+	cuisineCell.innerText = newFood.cuisine;
+	var dishCell = document.createElement("td");
+	dishCell.innerText = newFood.dish;
+	var weightCell = document.createElement("td");
+	weightCell.innerText = newFood.weight;
+
+	// combine cells and then append to table body
+	// var allCells = cuisineCell.outerHTML + dishCell.outerHTML + weightCell.outerHTML;
+	row.appendChild(cuisineCell);
+	row.appendChild(dishCell);
+	row.appendChild(weightCell);
+	var tbody = document.querySelector("tbody");
+	tbody.appendChild(row);
+});
